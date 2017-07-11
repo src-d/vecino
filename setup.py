237c6ae22f68d674b5d1e888ccdaf6b68576ac1e
Vadim Markovtsev <vadim@sourced.tech>
2017-07-11 11:14:01 +0200

    Add Travis CI and dummy tests

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="vecino",
+    description="Part of source{d}'s stack for machine learning on source "
+                "code. Provides API and tools to find similar GitHub repositories"
+                "based on source code identifiers.",
+    version="1.0.0",
+    license="Apache 2.0",
+    author="source{d}",
+    author_email="machine-learning@sourced.tech",
+    url="https://github.com/src-d/vecino",
+    download_url='https://github.com/src-d/vecino',
+    packages=find_packages(exclude=("vecino.tests",)),
+    keywords=["machine learning on source code", "word2vec", "id2vec",
+              "github", "swivel", "nbow", "bblfsh", "babelfish", "ast2vec"],
+    install_requires=["ast2vec>-1.0.0",
+                      "wmd>=1.2.4"],
+    package_data={"": ["LICENSE", "README.md"]},
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: POSIX",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Software Development :: Libraries"
+    ]
+)
