fcb9dea84d926d60ba368eb98b4366bdc01abdde
Vadim Markovtsev <vadim@sourced.tech>
2017-08-25 12:55:02 +0200

    Add the console entry

--- setup.py
+++ setup.py
@@ -6,13 +6,16 @@ setup(
     description="Part of source{d}'s stack for machine learning on source "
                 "code. Provides API and tools to find similar GitHub repositories"
                 "based on source code identifiers.",
-    version="0.1.0-alpha",
+    version="0.1.1-alpha",
     license="Apache 2.0",
     author="source{d}",
     author_email="machine-learning@sourced.tech",
     url="https://github.com/src-d/vecino",
     download_url="https://github.com/src-d/vecino",
     packages=find_packages(exclude=("vecino.tests",)),
+    entry_points={
+        "console_scripts": ["vecino=vecino.__main__:main"],
+    },
     keywords=["machine learning on source code", "word2vec", "id2vec",
               "github", "swivel", "nbow", "bblfsh", "babelfish", "ast2vec"],
     install_requires=["ast2vec>=0.1.0-alpha",
